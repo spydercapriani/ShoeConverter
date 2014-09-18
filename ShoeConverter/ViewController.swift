e3b64794708e8935fd02b3ff9a27272240933e56
Danny Gilbert <spydercapriani@gmail.com>
2014-09-18 13:53:53 -0500

    First Revision
    Single Button and Text field with switches for calculating shoe sizes
    of Men/Women
    
    NOTE: The math is incorrect as I don’t currently know what the
    conversion logic is.

--- ShoeConverter/ViewController.swift
+++ ShoeConverter/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var txtShoeSize: UITextField!
+    @IBOutlet weak var lblConvertedShoeSize: UILabel!
+    @IBOutlet weak var tglGender: UISwitch!
+    @IBOutlet weak var tglConversionType: UISwitch!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +24,38 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    @IBAction func btnConvertMensShoeSize(sender: UIButton) {
+        let MENS_EU_CONVERSION:Double = 30.5
+        let WOMENS_EU_CONVERSION:Double = 31
+        
+        var shoeSize = Double((txtShoeSize.text as NSString).doubleValue)
+        var strConversion:String
+        
+        txtShoeSize.resignFirstResponder()
+        lblConvertedShoeSize.hidden = false
+        lblConvertedShoeSize.textColor = UIColor.redColor()
+        
+        // Toggle conversion logic based on switches
+        if (tglConversionType.on){ // US to EU
+            strConversion = "EU Shoe Size: \t"
+            if (tglGender.on) { // Male
+                strConversion += "\(shoeSize + MENS_EU_CONVERSION)"
+            }else{ // Female
+                strConversion += "\(shoeSize + WOMENS_EU_CONVERSION)"
+            }
+        }else{ // EU to US
+            strConversion = "US Shoe Size: \t"
+            if (tglGender.on) { // Male
+                strConversion += "\(shoeSize - MENS_EU_CONVERSION)"
+            }else{ // Female
+                strConversion += "\(shoeSize - WOMENS_EU_CONVERSION)"
+            }
+        }
+        
+        
+        lblConvertedShoeSize.text = strConversion
+    }
 }
 
